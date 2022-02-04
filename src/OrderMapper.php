c9237d37aa07627a45b3477bc9f17dc3e3b7f019
Sergey Zaimovskii <zaymovskey@mail.ru>
2022-02-04 16:01:39 +0300

    order by date

--- src/OrderMapper.php
+++ src/OrderMapper.php
@@ -16,7 +16,7 @@ class OrderMapper
 
     public function getAll(): array
     {
-        $sql = 'SELECT orders.*, products.* FROM orders INNER JOIN products ON product_id = products.id';
+        $sql = 'SELECT orders.*, products.* FROM orders INNER JOIN products ON product_id = products.id ORDER BY at_create';
         $statement = $this->connection->prepare($sql);
         $statement->execute();
         return $statement->fetchAll();
